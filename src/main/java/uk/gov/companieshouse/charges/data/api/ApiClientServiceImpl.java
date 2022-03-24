ffaffcac17428049121cbc39e3f37ef783ae1e13
gianmarios-ch <gseregni@companieshouse.gov.uk>
2022-03-24 16:40:33 +0000

    dsnd-542 - Addressed issues with API Key

--- src/main/java/uk/gov/companieshouse/charges/data/api/ApiClientServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/charges/data/api/ApiClientServiceImpl.java
@@ -1,14 +1,32 @@
 package uk.gov.companieshouse.charges.data.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.InternalApiClient;
-import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
+import uk.gov.companieshouse.api.http.ApiKeyHttpClient;
+import uk.gov.companieshouse.api.http.HttpClient;
 
 @Component
 public class ApiClientServiceImpl implements ApiClientService {
 
+    @Value("${chs.kafka.api.key}")
+    private String chsApiKey;
+
+    @Value("${chs.kafka.api.endpoint}")
+    private String internalApiUrl;
+
     @Override
     public InternalApiClient getInternalApiClient() {
-        return ApiSdkManager.getPrivateSDK();
+        InternalApiClient internalApiClient = new InternalApiClient(getHttpClient());
+        internalApiClient.setInternalBasePath(internalApiUrl);
+        internalApiClient.setBasePath(internalApiUrl);
+
+        return internalApiClient;
+    }
+
+
+    private HttpClient getHttpClient() {
+        ApiKeyHttpClient httpClient = new ApiKeyHttpClient(chsApiKey);
+        return httpClient;
     }
 }
