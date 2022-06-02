ebf3cb9163d82324f23584950da5a3c81d4530ab
zahidm3 <zmuhammad@companieshouse.gov.uk>
2022-06-02 15:45:55 +0100

    Bug fix DSND-1046 to remove empty scottish alterations object

---
+++ src/main/java/uk/gov/companieshouse/charges/data/serialization/NonBlankStringSerializer.java
@@ -0,0 +1,26 @@
+package uk.gov.companieshouse.charges.data.serialization;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import org.apache.commons.lang.StringUtils;
+
+public class NonBlankStringSerializer extends JsonSerializer<String> {
+
+    @Override
+    public void serialize(String value, JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider) throws IOException {
+        if (isEmpty(serializerProvider, value)) {
+            jsonGenerator.writeNull();
+        } else {
+            jsonGenerator.writeString(value);
+        }
+    }
+
+    @Override
+    public boolean isEmpty(SerializerProvider provider, String value) {
+        return StringUtils.isBlank(value);
+    }
+}
---
+++ src/test/java/uk/gov/companieshouse/charges/data/serialization/ScottishAlterationsApiSerializerTest.java
@@ -0,0 +1,59 @@
+package uk.gov.companieshouse.charges.data.serialization;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.charges.ScottishAlterationsApi;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ScottishAlterationsApiSerializerTest {
+
+    @Mock
+    private SerializerProvider serializerProvider;
+    @Mock
+    private JsonGenerator jsonGenerator;
+
+    private ScottishAlterationsApiSerializer apiSerializer;
+
+    @BeforeEach
+    void setUp() {
+        apiSerializer = new ScottishAlterationsApiSerializer();
+    }
+
+    @Test
+    void testSerialiseScottishAlterationsApiObject() throws IOException {
+        ScottishAlterationsApi alterationsApi = new ScottishAlterationsApi();
+        alterationsApi.setDescription("test");
+        apiSerializer.serialize(alterationsApi, jsonGenerator, serializerProvider);
+
+        verify(jsonGenerator).writeRawValue("{\"description\":\"test\"}");
+    }
+
+    @Test
+    void testEmptyScottishAlterationsApiObject() {
+        ScottishAlterationsApi alterationsApi = new ScottishAlterationsApi();
+
+        assertTrue(apiSerializer.isEmpty(serializerProvider, alterationsApi));
+        assertTrue(apiSerializer.isEmpty(serializerProvider, null));
+    }
+
+    @Test
+    void testNonEmptyScottishAlterationsApiObject() {
+        ScottishAlterationsApi alterationsApi = new ScottishAlterationsApi();
+        alterationsApi.setDescription("some description");
+        alterationsApi.setHasAlterationsToProhibitions(true);
+
+        assertFalse(apiSerializer.isEmpty(serializerProvider, alterationsApi));
+    }
+
+}
