efdd3249eba59fa430c2cbafbd32bb1041961caa
chillier-ch <chillier@companieshouse.gov.uk>
2024-04-30 16:10:02 +0100

    Update log config

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -62,7 +62,7 @@ locals {
   task_secrets = concat(local.global_secret_list,local.service_secret_list)
 
   task_environment = concat(local.ssm_global_version_map,local.ssm_service_version_map,[
-    { "name" : "LOGLEVEL", "value" : var.log_level }
+    { "name" : "PORT", "value" : local.container_port }
   ])
 
 # get eric secrets from global secrets map
