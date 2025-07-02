897cfd49809d20a94b97e69b1ad6891dc3350c03
scunnahhyland-ch <scunnahhyland@companieshouse.gov.uk>
2025-07-02 09:15:37 +0100

    tech debt updates for platform side

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.294"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.333"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
@@ -28,7 +28,7 @@ module "secrets" {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.294"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.333"
 
   # Environmental configuration
   environment             = var.environment
@@ -39,18 +39,18 @@ module "ecs-service" {
   task_execution_role_arn = data.aws_iam_role.ecs_cluster_iam_role.arn
 
   # Load balancer configuration
-  lb_listener_arn                   = data.aws_lb_listener.service_lb_listener.arn
-  lb_listener_rule_priority         = local.lb_listener_rule_priority
-  lb_listener_paths                 = local.lb_listener_paths
-  multilb_setup                   = true
-  multilb_listeners               = {
-    "priv-api-lb": {
-      listener_arn                = data.aws_lb_listener.secondary_lb_listener.arn,
-      load_balancer_arn           = data.aws_lb.secondary_lb.arn
+  lb_listener_arn           = data.aws_lb_listener.service_lb_listener.arn
+  lb_listener_rule_priority = local.lb_listener_rule_priority
+  lb_listener_paths         = local.lb_listener_paths
+  multilb_setup             = true
+  multilb_listeners = {
+    "priv-api-lb" : {
+      listener_arn      = data.aws_lb_listener.secondary_lb_listener.arn,
+      load_balancer_arn = data.aws_lb.secondary_lb.arn
     }
-    "pub-api-lb": {
-      load_balancer_arn      = data.aws_lb.service_lb.arn
-      listener_arn           = data.aws_lb_listener.service_lb_listener.arn
+    "pub-api-lb" : {
+      load_balancer_arn = data.aws_lb.service_lb.arn
+      listener_arn      = data.aws_lb_listener.service_lb_listener.arn
     }
   }
 
